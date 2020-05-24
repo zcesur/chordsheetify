bd77df1929e288b3c2b31ed55a65573c759bad27
zcesur <17045339+zcesur@users.noreply.github.com>
2020-05-24 22:41:18 +0300

    Parse sheet

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, text, textarea)
+import Chords exposing (Token(..))
+import Html exposing (Html, b, div, span, text, textarea)
 import Html.Attributes exposing (class, placeholder, value)
 import Html.Events exposing (onInput)
+import Parser exposing (..)
 
 
 
@@ -18,13 +20,25 @@ main =
 -- MODEL
 
 
+type alias RawSheet =
+    String
+
+
+type alias ParsedLine =
+    Result (List DeadEnd) (List Token)
+
+
+type alias ParsedSheet =
+    List ParsedLine
+
+
 type alias Model =
-    { content : String }
+    { input : RawSheet, output : ParsedSheet }
 
 
 init : Model
 init =
-    { content = "" }
+    { input = "", output = [] }
 
 
 
@@ -38,17 +52,37 @@ type Msg
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Change newContent ->
-            { model | content = newContent }
+        Change i ->
+            { model | input = i, output = Chords.parseSheet i }
 
 
 
 -- VIEW
 
 
+renderLine : ParsedLine -> Html Msg
+renderLine res =
+    case res of
+        Ok ts ->
+            div [] (List.map renderToken ts)
+
+        Err e ->
+            span [] [ text (deadEndsToString e) ]
+
+
+renderToken : Token -> Html Msg
+renderToken t =
+    case t of
+        Lyrics s ->
+            span [] [ text s ]
+
+        Parsed c ->
+            span [] [ b [] [ text (Chords.toString c) ] ]
+
+
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ textarea [ class "sheet-input", placeholder "Sheet", value model.content, onInput Change ] []
-        , div [ class "sheet-output" ] [ text model.content ]
+        [ textarea [ class "sheet-input", placeholder "Sheet", value model.input, onInput Change ] []
+        , div [ class "sheet-output" ] (List.map renderLine model.output)
         ]
