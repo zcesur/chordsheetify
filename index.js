2c902febe1d7b19be6c676ae2ebe63eb1dc8bb07
zcesur <17045339+zcesur@users.noreply.github.com>
2020-05-25 04:48:53 +0300

    Add webpack and a bunch of essential loaders & plugins

---
+++ index.js
@@ -0,0 +1,16 @@
+import "normalize.css/normalize.css";
+import "milligram/dist/milligram.css";
+import "./src/styles/index.scss";
+
+try {
+  const app = Elm.Main.init({ node: document.getElementById("elm") });
+} catch (e) {
+  // display initialization errors (e.g. bad flags, infinite recursion)
+  const header = document.createElement("h1");
+  header.style.fontFamily = "monospace";
+  header.innerText = "Initialization Error";
+  const pre = document.getElementById("elm");
+  document.body.insertBefore(header, pre);
+  pre.innerText = e;
+  throw e;
+}
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+
+const TerserJSPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      inject: "body"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Runs compiled CSS through postcss for vendor prefixing
+          "postcss-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Runs compiled CSS through postcss for vendor prefixing
+          "postcss-loader"
+        ]
+      }
+    ]
+  }
+};
